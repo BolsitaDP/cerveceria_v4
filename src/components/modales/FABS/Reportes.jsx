1ec42441815d83ca2bd4508480dd7735684146c1
Santiago <sgiraldo118@gmail.com>
2023-11-22 14:35:40 -0500

    El llenado de la  grid de reportes se hace mal

--- src/components/modales/FABS/Reportes.jsx
+++ src/components/modales/FABS/Reportes.jsx
@@ -13,11 +13,14 @@ import {
 } from "@mui/material";
 import { useTheme } from "@emotion/react";
 import { useSelector } from "react-redux";
+import { DataGrid, esES } from "@mui/x-data-grid";
 
 const Reportes = () => {
-  const [productoABuscar, setProductoABuscar] = useState("");
+  const [productoABuscar, setProductoABuscar] = useState([]);
 
-  const [productosSelect, setProductosSelect] = useState([]);
+  const [elementosEnGrid, setElementosEnGrid] = useState([]);
+
+  // const [productosSelect, setProductosSelect] = useState([]);
 
   const fechasSeleccionadas = useSelector((state) => state.dates.selectedDates);
   const contenedoresEstado = useSelector((state) => state.contenedores);
@@ -25,26 +28,69 @@ const Reportes = () => {
 
   const theme = useTheme();
 
-  let productos = {};
-  let nombresUnicos = new Set();
+  let productosNombres = [];
 
-  useEffect(() => {
+  Object.values(contenedoresEstado.calendario).forEach((salon) => {
+    Object.values(salon.dias).forEach((dia) => {
+      if (fechasSeleccionadas.includes(dia.fecha)) {
+        if (dia.contenido.length > 0) {
+          let contenidoDia = dia.contenido;
+          contenidoDia.forEach((el) => {
+            if (!productosNombres.includes(el.producto)) {
+              productosNombres.push(el.producto);
+            }
+          });
+        }
+      }
+    });
+  });
+
+  const handleChangeProduct = (newValue) => {
+    console.log(newValue);
+
+    let newRows = [];
+
     Object.values(contenedoresEstado.calendario).forEach((salon) => {
       Object.values(salon.dias).forEach((dia) => {
         if (fechasSeleccionadas.includes(dia.fecha)) {
-          dia.contenido.forEach((el) => {
-            if (el.codigoNombre) {
-              // TODO: Poner el array para las opciones
-            }
-          });
+          if (dia.contenido.length > 0) {
+            let contenidoDia = dia.contenido;
+            contenidoDia.forEach((el) => {
+              if (productoABuscar.includes(el.producto)) {
+                newRows.push(el);
+              }
+            });
+          }
         }
       });
     });
 
-    setProductosSelect(productos);
+    // TODO: El valor se settea en dos ticks, quizá haciendo que la lectura y discriminación se haga desde arriba (?)
+    setElementosEnGrid(newRows);
+  };
 
-    console.log(productos);
-  }, []);
+  console.log(elementosEnGrid);
+
+  let columns = [
+    {
+      field: "producto",
+      headerName: "Producto",
+      flex: 1,
+      minWidth: 250,
+    },
+    {
+      field: "cantidad",
+      headerName: "Cantidad",
+      flex: 1,
+      minWidth: 250,
+    },
+    {
+      field: "salonProgramado",
+      headerName: "Salón programado",
+      flex: 1,
+      minWidth: 250,
+    },
+  ];
 
   return (
     <BasicModal titulo={"Reportes por productos"}>
@@ -57,19 +103,23 @@ const Reportes = () => {
           justifyContent: "center",
           alignItems: "center",
           gap: "10px",
+          maxHeight: "70px",
           backgroundColor: theme.palette.primary.main,
         }}>
         <Autocomplete
           disablePortal
           multiple
           value={productoABuscar}
-          getOptionLabel={(option) => option.producto}
           onChange={(event, newValue) => {
             setProductoABuscar(newValue);
+            handleChangeProduct(newValue);
           }}
           id="combo-box-demo"
-          options={productosSelect}
-          sx={{ width: "50%" }}
+          options={productosNombres}
+          sx={{
+            minWidth: "50%",
+            "& .MuiAutocomplete-tag": { flexWrap: "nowrap" },
+          }}
           renderInput={(params) => (
             <TextField
               {...params}
@@ -83,9 +133,28 @@ const Reportes = () => {
       </Box>
       <Box
         sx={{
-          height: "60vh",
-          width: "60vw",
-        }}></Box>
+          display: "flex",
+          minHeight: "180px",
+          height: "50vh",
+          width: "100%",
+          // height: "60vh",
+          // width: "60vw",
+        }}>
+        <Box sx={{ flexGrow: 1, width: "100%" }}>
+          <DataGrid
+            localeText={esES.components.MuiDataGrid.defaultProps.localeText}
+            rows={elementosEnGrid}
+            pageSizeOptions={[10]}
+            initialState={{
+              pagination: {
+                paginationModel: { page: 0, pageSize: 10 },
+              },
+            }}
+            columns={columns}
+            getRowId={(row) => row.id}
+          />
+        </Box>
+      </Box>
     </BasicModal>
   );
 };
