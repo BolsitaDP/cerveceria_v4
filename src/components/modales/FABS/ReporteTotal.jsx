5d3dd72477f26e98f33041896c64ede0fe4c2d50
Santiago <sgiraldo118@gmail.com>
2024-02-09 14:24:45 -0500

    Todoes actualizados y tamaño de reportes en pequeño, corregir en switch

--- src/components/modales/FABS/ReporteTotal.jsx
+++ src/components/modales/FABS/ReporteTotal.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import BasicModal from "../../MUIComponents/BasicModal";
 import { Box } from "@mui/material";
 import { DataGrid, GridToolbar, esES } from "@mui/x-data-grid";
@@ -6,6 +6,10 @@ import { useSelector } from "react-redux";
 import { v4 as uuid } from "uuid";
 
 const ReporteTotal = () => {
+  const [switchColoresPorProductos, setSwitchColoresPorProductos] =
+    useState(false);
+  const [switchTamanoPequeno, setSwitchTamanoPequeno] = useState(false);
+
   const contenedoresEstado = useSelector((state) => state.contenedores);
 
   let salones = contenedoresEstado.calendario;
@@ -20,6 +24,12 @@ const ReporteTotal = () => {
       diasFechas.push(x);
     });
 
+  let nombreSalones = [];
+
+  Object.keys(salones).forEach((salon) => {
+    nombreSalones.push(salon);
+  });
+
   let columns = [
     {
       field: "dia",
@@ -29,37 +39,70 @@ const ReporteTotal = () => {
         let [nombre, fecha] = row.dia.split("&");
         let [dia, mes] = fecha.split("/");
 
-        return `${nombre} ${dia}/${mes}`;
+        return (
+          <span style={{ fontSize: "8px" }}>{`${nombre} ${dia}/${mes}`}</span>
+        );
       },
     },
     ...Object.keys(salones).map((salon, index) => ({
       field: salon,
       headerName: salon,
-      width: 100,
+      width: 90,
       renderCell: (row) => {
         let salon = row.field;
-        row[salon];
+        let contenido = row.row[salon];
+        return (
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              gap: "10px",
+              margin: "5px 0",
+              fontSize: "8px",
+            }}>
+            {contenido.map((sol, index) => (
+              <div
+                key={index}
+                style={{
+                  backgroundColor:
+                    sol.tipoRequerimiento === "PRODUCCIÓN LOCAL"
+                      ? "rgba(92, 101, 192, 0.4)"
+                      : "rgba(240, 93, 103, 0.4)",
+                  padding: "5px",
+                  borderRadius: "5px",
+                }}>
+                {sol.producto} ({sol.codigoNombre})
+                <br />
+                <strong>
+                  {sol.cantidad} {sol.unidadMedida}
+                </strong>
+              </div>
+            ))}
+          </div>
+        );
       },
     })),
     {
       field: "total",
       headerName: "Total",
       width: 150,
+      renderCell: ({ row }) => {
+        // console.log(row);
+      },
     },
   ];
 
   let rows7 = new Array(7);
 
-  let nombreSalones = [];
+  rows7 = diasFechas.map((dia, index) => {
+    nombreSalones.map((salon) => console.log(salon));
+    // El total se tiene que poner desde acá en un propiedad "total" que es la que se renderiza
 
-  Object.keys(salones).forEach((salon) => {
-    nombreSalones.push(salon);
-  });
-
-  rows7 = diasFechas.map((dia) => {
     let fullData = {
       dia,
       salones: nombreSalones.map((salon) => salon),
+      index,
+      total: "s",
     };
 
     nombreSalones.forEach((salon) => {
@@ -71,11 +114,6 @@ const ReporteTotal = () => {
     return fullData;
   });
 
-  console.log(rows7);
-  console.log(nombreSalones);
-  console.log(columns);
-  console.log(salones);
-
   return (
     <BasicModal titulo={"Reporte total"}>
       <Box
@@ -85,7 +123,7 @@ const ReporteTotal = () => {
           justifyContent: "center",
           alignItems: "center",
           gap: "10px",
-          height: "60vh",
+          height: "80vh",
           width: "max-content",
           maxWidth: "80vw",
         }}>
@@ -99,6 +137,7 @@ const ReporteTotal = () => {
               paginationModel: { page: 0, pageSize: 7 },
             },
           }}
+          getRowHeight={() => "auto"}
           columns={columns}
           getRowId={(row) => uuid()}
         />
