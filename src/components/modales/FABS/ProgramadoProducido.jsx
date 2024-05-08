96fd2f01e0e459cd9621337e4d16918447bf0104
Santiago <sgiraldo118@gmail.com>
2024-05-08 11:31:19 -0500

    Se quita la opció de exportar externa a la tabla de prog/prod

--- src/components/modales/FABS/ProgramadoProducido.jsx
+++ src/components/modales/FABS/ProgramadoProducido.jsx
@@ -105,7 +105,7 @@ const ProgramadoProducido = () => {
   return (
     <BasicModal
       titulo={"Requerido, programado y producido"}
-      exportar
+      // exportar
       funcionAlDarClickExportar={handleExportar}>
       <Box
         sx={{
